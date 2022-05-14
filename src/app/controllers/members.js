6e7583140cd3315743856609fa15e547c8239956
Roberto Luiz <luizroberto1957@hotmail.com>
2022-05-14 19:27:55 -0300

    Add DB in Members

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,11 +1,20 @@
 const {age, date} = require('../../lib/utils')
+const Member = require('../models/Member')
 
 module.exports = {
   index(req, res){
-    return res.render("members/index")
+
+    Member.all(function(members){
+      return res.render("members/index", {members})
+    })
+
+    
   },
   create(req, res){
-    return res.render("members/create")
+    Member.instructorsSelectOptions(function(options){
+      return res.render("members/create", {instructorOptions: options})
+    })
+    
   },
   post(req, res){
     const keys = Object.keys(req.body)
@@ -15,101 +24,49 @@ module.exports = {
         return res.send('Please, fill all fields!')      
       }
     }
-    let {avatar_url, birth, name, services, gender} = req.body
-  
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.members.length + 1)
-  
-    data.members.push({
-      id,
-      name,
-      avatar_url,
-      birth,
-      gender,
-      services,
-      created_at,
+    Member.create(req.body, function(member){
+      return res.redirect(`/members/${member.id}`)
     })
-  
-    fs.writeFile("data.json", JSON.stringify(data,  null, 2), function(err){
-      if (err) return res.send("Write file error!") 
-  
-      return res.redirect("/members")
-    })
-    // return res.send(req.body)
+ 
   },
   show(req, res){  
-    //req.params
-    const { id } = req.params
-    const foundInstructor = data.members.find(function(member) {
-      return member.id == id
+    Member.find(req.params.id, function(member){
+      if (!member) return res.send("Member not found")
+
+      member.birth = date(member.birth).birthDay
+
+      return res.render("members/show", {member})
     })
-    if (!foundInstructor) return res.send("Instructor not found!")
-
-    const member = {
-      ...foundInstructor,
-      age: age(foundInstructor.birth),
-      services: foundInstructor.services.split(","),
-      created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
-    }
-
-    return res.render("members/show", {member})
   },
   edit(req, res){  
-    //req.params
-    const { id } = req.params
-    const foundInstructor = data.members.find(function(member) {
-      return member.id == id
+    Member.find(req.params.id, function(member){
+      if (!member) return res.send("Member not found")
+
+      member.birth = date(member.birth).iso
+      
+      Member.instructorsSelectOptions(function(options){
+        //return res.render("members/create", {instructorOptions: options})
+        return res.render("members/edit", {member, instructorOptions: options})
+      })
+      
     })
-    if (!foundInstructor) return res.send("Instructor not found!")
-  
-    const member ={
-      ...foundInstructor,
-      birth: date(foundInstructor.birth).iso
-    }
-  
-    return res.render('members/edit', {member}) 
   },
   put(req, res){
-    const { id } = req.body
-    let index = 0
+    const keys = Object.keys(req.body)
 
-    const foundInstructor = data.members.find(function(member, foundIndex) {
-      if(id == member.id){
-          index = foundIndex
-          return true
+    for(key of keys) {
+      if (req.body[key] == "") {
+        return res.send('Please, fill all fields!')      
       }
-    })
-    //console.log(index)
-    if (!foundInstructor) return res.send("Instructor not found!")
-
-    const member = {
-      ...foundInstructor,
-      ...req.body,
-      birth: Date.parse(req.body.birth),
-      id: Number(req.body.id)
     }
-    data.members[index] = member
 
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err){
-      if (err) return res.send("Write error!")
-
-      return res.redirect(`/members/${id}`)
+    Member.update(req.body, function(){
+      return res.redirect(`/members/${req.body.id}`)
     })
   },
   delete(req, res){
-    const {id} = req.body
-
-    const filteredInstructors = data.members.filter(function(member){
-      return member.id != id
+    Member.delete(req.body.id, function(){
+      return res.redirect(`/members`)
     })
-  
-    data.members = filteredInstructors
-  
-    fs.writeFile("data.json", JSON.stringify(data,  null, 2), function (err){
-      if (err) return res.send("Write file error!")
-  
-      return res.redirect("/members")
-    })
-  }
+  },
 }
--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -5,11 +5,9 @@ const db = require('../../config/db')
 module.exports = {
   all(callback) {
     db.query(`
-    SELECT instructors.*, count(members) AS total_students
-    FROM instructors
-    LEFT JOIN members ON (instructors.id = members.instructor_id)
-    GROUP BY instructors.id
-    ORDER BY total_students DESC`, function(err,results){
+    SELECT * 
+    FROM members
+    ORDER BY name ASC`, function(err,results){
       if(err) throw `Database Error! ${err}`
 
       callback(results.rows)
@@ -17,14 +15,17 @@ module.exports = {
   },
   create(data, callback) {
     const query = `
-    INSERT INTO instructors (
+    INSERT INTO members (
       name,
       avatar_url,
       gender,
-      services,
+      email,
       birth,
-      created_at
-    ) VALUES ($1, $2, $3, $4, $5, $6)
+      blood,
+      weight,
+      height,
+      instructor_id
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
       RETURNING id
     `
   
@@ -32,9 +33,12 @@ module.exports = {
       data.name,
       data.avatar_url,
       data.gender,
-      data.services,
+      data.email,
       date(data.birth).iso,
-      date(Date.now()).iso
+      data.blood,
+      data.weight,
+      data.height,
+      data.instructor
     ]
 
     db.query(query, values,function(err,results){
@@ -46,22 +50,27 @@ module.exports = {
   },
   find(id, callback) {
     db.query(`
-      SELECT *
-      FROM instructors
-      WHERE id = $1`, [id], function(err, results){
+      SELECT members.*, instructors.name AS instructor_name
+      FROM members
+      LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+      WHERE members.id = $1`, [id], function(err, results){
         if(err) throw `Database Error! ${err}`
         callback(results.rows[0])
     })
   },
   update(data, callback) {
     const query = `
-    UPDATE instructors SET
+    UPDATE members SET
       avatar_url=($1),
       name=($2),
       birth=($3),
       gender=($4),
-      services=($5)
-    WHERE id = $6
+      email=($5),
+      blood=($6),
+      weight=($7),
+      height=($8),
+      instructor_id=($9)
+    WHERE id = $10
     `
 
     const values = [
@@ -69,7 +78,11 @@ module.exports = {
       data.name,
       date(data.birth).iso,
       data.gender,
-      data.services,
+      data.email,
+      data.blood,
+      data.weight,
+      data.height,
+      data.instructor,
       data.id
     ]
     db.query(query, values, function(err, results){
@@ -78,10 +91,17 @@ module.exports = {
     })
   },
   delete(id, callback){
-    db.query(`DELETE FROM instructors WHERE id = $1`, [id], function(err, results){
+    db.query(`DELETE FROM members WHERE id = $1`, [id], function(err, results){
       if(err) throw `Database Error! ${err}`
 
       return callback()
+    })
+  },
+  instructorsSelectOptions(callback){
+    db.query(`SELECT name, id FROM instructors`, function(err, results){
+      if(err) throw `Database Error! ${err}`
+
+      return callback(results.rows)
     })
   }
 }
